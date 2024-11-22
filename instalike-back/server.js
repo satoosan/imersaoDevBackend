76dca889219a59868761ff81ec26a1b9309bcf9d
satoosan <satogui5+github@gmail.com>
2024-11-21 21:16:14 -0300

    estrutura primeira api com get e banco de dados

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,20 +1,42 @@
 import express from "express";
 
+const posts = [
+  { 
+    id: 1, 
+    descricao: "Uma foto teste", 
+    image: "https://placecats.com/millie/300/150",
+  },
+  { 
+    id: 2, 
+    descricao: "Gato brincando com um novelo de lã", 
+    image: "https://placecats.com/millie/300/150" 
+  },
+  {
+    id: 3, 
+    descricao: "Gato comendo uma lasanha", 
+    image: "https://placecats.com/millie/300/150" 
+  }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Servidor escutando..");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Boas vindas à Imersão!");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
 });
 
-// app.get("/livro", (req, res) => {
-//   res.status(200).send({
-//     "titulo": "O Senhor dos Anéis",
-//     "autor": "J.R.R. Tolkien",
-//     "ano": 1954,
-//     "genero": "Fantasia"
-//   });
-// });
+function buscarPostPorID(id){
+  return posts.findIndex((post)=>{
+    return post.id === Number(id)
+  })
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id)
+  res.status(200).json(posts[index]);
+});
 
