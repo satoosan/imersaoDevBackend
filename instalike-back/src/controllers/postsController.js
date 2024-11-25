16884107ebfef32e6fc4ee984ac48f6274d7ba4c
satoosan <satogui5+github@gmail.com>
2024-11-24 23:49:02 -0300

    api ok

--- instalike-back/src/controllers/postsController.js
+++ instalike-back/src/controllers/postsController.js
@@ -29,8 +29,8 @@ export async function uploadImagem(req, res) {
 
     try {
         const postCriado = await criarPost(novoPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
-        fs.renameSync(req.file.path, imagemAtualizada)
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
         res.status(200).json(postCriado);  
     } catch(erro) {
         console.error(erro.message);
